3951a5bed1170d4ae92e0c6d581da2221e687115
coderrakibul <coder.rakibul@gmail.com>
2022-04-01 01:46:06 +0600

    first commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,6 @@ import './Cart.css';
 
 const Cart = (props) => {
     const { cart } = props;
-    // console.log(cart);
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -22,6 +21,7 @@ const Cart = (props) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: {tax}</p>
             <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
+            <p>{props.children}</p>
         </div>
     );
 };
